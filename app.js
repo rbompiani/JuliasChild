05885735fd51cbfdbaea1ccf924bd5cafeee06cc
stbrooks <stephanie.brooks@outlook.com>
2019-07-22 14:05:58 -0700

    update

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var token = "fa82f5e36dddbeae1a09cb7e2df0a6d5";
 var appID = "ff691d51";
-var edamam_url = "https://api.yelp.com/v3/businesses/search?";
+var edamam_url = "https://api.edamam.com/search?";
 
 function get_recipe(queryURL){
     $.ajax({url: queryURL, method: "GET", headers: {Authorization: token}}).done(function(edamam_data){
-       console.log(yelp_data);
+       console.log(edamam_data);
 
-       for (var i=0; i<edamam_data.businesses.length;i++){
+       for (var i=0; i<edamam_data.hits.length;i++){
         console.log(edamam_data.hits[i].recipe.label);
        }
 
