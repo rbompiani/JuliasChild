ba33625e3fda806e8f572710a29767f127e2da43
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-07-26 23:26:55 -0700

    add seeds.sql

---
+++ models/seeds.sql
@@ -0,0 +1,52 @@
+INSERT INTO recipe (recipeImage, recipeTitle, recipeDesc, calories, nutrition, instructions)
+VALUES ('https://www.teatimemagazine.com/wp-content/uploads/2014/09/Cherry-Rose-Scones.jpg', 'Cherry-Rose Scones', 'Breakfast', NULL, NULL, "1. Preheat oven to 375˚. Line two baking sheets with parchment paper; set aside.
+
+2. In a large bowl, combine flour, ½ cup sugar, baking powder, and salt. Using a pastry blender, cut in butter and cream cheese until mixture is crumbly. Add dehydrated cherries, and stir to combine. Combine cream, rose water, cherry extract, and vanilla extract. Add cream mixture to flour mixture, and stir just until dry ingredients are moistened.
+
+3. On a lightly floured surface, roll dough to ¾-inch thickness. Using a 2-inch round cutter, cut scones. Place scones on prepared baking sheets, and sprinkle evenly with remaining sugar. Bake for 13 to 15 minutes, or until light golden brown."),
+       ('https://www.dinersdriveinsdiveslocations.com/gruyere-and-black-pepper-popovers.jpg', 'Gruyère and Black Pepper Popovers', 'Savory Baked Goods', NULL, NULL, "1. Preheat the oven to 375˚ and position a rack in the bottom third. Place the popover pan in the oven to warm.
+
+2. In a small saucepan, warm the milk over medium heat until it is hot (about 125˚) but not boiling. Remove from the heat. In a large bowl, whisk the eggs with the salt and black pepper until smooth. Slowly whisk in the warm milk. Add the flour and whisk until the batter is just combined. (It may be slightly lumpy.)
+
+3. Remove the popover pan from the oven. Spray the pan generously with nonstick cooking spray. Pour about ⅓ cup of the batter into each of 6 popover cups. Set 1 or 2 cubes of the cheese on top of the batter in each cup.
+
+4. Bake the popovers until puffed and deep golden brown, about 40 minutes (do not open the door or they may collapse). Remove the popovers from the pans, sprinkle with the grated Gruyère and serve immediately.
+
+"),
+       ('https://www.dinneratthezoo.com/wp-content/uploads/2016/08/baked-buffalo-wings-11.jpg', 'Classic Buffalo Wings', 'Chicken', NULL, NULL, "1. Preheat oven to 400° and place a wire rack over a baking sheet. In a large bowl, toss chicken wings with oil and season with garlic powder, salt, and pepper. Transfer to prepared baking sheet.
+
+2. Bake until chicken is golden and skin is crispy, 50 to 60 minutes, flipping the wings halfway through.
+
+3. In a small saucepan, whisk together hot sauce and honey. Bring to simmer then stir in butter. Cook until butter is melted and slightly reduced, about 2 minutes. Heat broiler on low. Transfer baked wings to a bowl and toss with buffalo sauce until completely coated. Return wings to rack and broil—watching carefully!—until sauce caramelizes, 3 minutes. Serve with ranch dressing and vegetables.");   
+
+
+INSERT INTO ingredients (ingredientLines, recipeID)
+VALUES ("2½ cups all-purpose flour", 1),
+       ("½ cup plus 2 tablespoons sugar, divided", 1),
+       ("2 teaspoons baking powder", 1),
+       ("½ teaspoon salt", 1),
+       ("½ cup cold unsalted butter, cubed", 1),
+       ("1 (3-ounce) package cold cream cheese, cubed", 1),
+       ("1 (2-ounce) package dehydrated cherries*, finely chopped", 1),
+       ("⅓ cup heavy cream", 1),
+       ("1 teaspoon rose water", 1),
+       ("¼ teaspoon cherry extract", 1),
+       ("½ teaspoon vanilla extract", 1),
+       ("2 cups whole milk", 2),
+       ("4 large eggs", 2),
+       ("1 1/2 teaspoons salt", 2),
+       ("1/2 teaspoon freshly ground black pepper", 2),
+       ("2 cups all-purpose flour", 2),
+       ("2 ounces Gruyère cheese, cut into 16 small cubes, plus freshly grated cheese for garnish", 2),
+       ("2 lb. chicken wings", 3),
+       ("2 tbsp. Vegetable oil", 3),
+       ("1 tsp. garlic powder", 3),
+       ("Kosher salt", 3),
+       ("Freshly ground black pepper", 3),
+       ("1/4 c. hot sauce (such as Cholula)", 3),
+       ("2 tbsp. honey", 3),
+       ("4 tbsp. butter", 3),
+       ("Ranch dressing, for serving", 3),
+       ("Carrot sticks, for serving", 3),
+       ("Celery sticks, for serving", 3);
+
