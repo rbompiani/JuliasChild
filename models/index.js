4c70a931a49214cd1ae5e2704008741e843c07da
SamanthaLossett <samanthalossett@gmail.com>
2019-07-31 11:01:49 -0700

    index.js added lines 16 and 20

--- models/index.js
+++ models/index.js
@@ -13,9 +13,12 @@ var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
+var mysql= require("mysql");
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: "mysql"
+  });
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
